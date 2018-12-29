0b384e623d29ac6a6398b120b2f59cf449c8f04c
山下功介 <toxicsound.ksk@gmail.com>
2018-12-29 14:30:33 +0700

    graphqlの雛形

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import mongoose from 'mongoose';
+
+import schema from './schema';
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/gql_db', { useNewUrlParser: true });
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  return res.send('Hello GraphQL!');
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+  context: {
+    user: 1,
+  }
+}))
+
+app.listen(PORT, () => {
+  console.log(`Server is Running at PORT ${PORT}`);
+})
---
+++ src/resolvers.js
@@ -0,0 +1,9 @@
+export const resolvers = {
+  Query: {
+    hello(parent, args, ctx, info) {
+      console.log(ctx.user);
+      console.log(info);
+      return args.msg;
+    }
+  }
+}
